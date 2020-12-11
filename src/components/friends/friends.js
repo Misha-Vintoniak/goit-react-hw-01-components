715deac4a94cca1a539c77b5b5617e7583bfc7bd
Misha-Vintoniak <63545726+Misha-Vintoniak@users.noreply.github.com>
2020-12-11 02:28:35 +0200

    remove components

--- src/components/friends/friends.js
+++
@@ -1,6 +0,0 @@
-import s from './Friends.module.css';
-import FriendListItem from './FriendListItem';
-
-export default function Friends({ friends }) {
-  return <ul className={s.friendList}>{friends.map(FriendListItem)}</ul>;
-}
